c18e3494227b0097945347ca7bad645faabe6161
Kartik Sharma <poojan4604@gmail.com>
2026-02-16 10:23:21 +0530

    transport functions

--- app/mediasoup/mediasoup.ts
+++ app/mediasoup/mediasoup.ts
@@ -13,63 +13,19 @@ let rtpParams: mediasoup.types.RtpParameters;
 //asks whaat codecs the machines supports
 const rtpCapabilities = await mediasoup.getSupportedRtpCapabilities();
 
-const worker=await mediasoup.createWorker({
-    logLevel:'error',
-    rtcMinPort:10000, //Minimun RTC port for ICE, DTLS, RTP, etc.
-    rtcMaxPort:59999, //Maximum RTC port for ICE, DTLS, RTP, etc.
-});
+
 
 
 //These are the only codecs allowed inside this SFU room.
-const mediaCodecs: RtpCodecCapability[] = [
-    {
-        kind: "audio",
-        mimeType: "audio/opus",
-        clockRate: 48000,
-        channels: 2,
-        preferredPayloadType: 0
-    },
-    {
-        kind: "video",
-        mimeType: "video/VP8",
-        clockRate: 90000,
-        preferredPayloadType: 0
-    },
-    {
-        kind: "video",
-        mimeType: "video/H264",
-        clockRate: 90000,
-        parameters: {
-            "packetization-mode": 1,
-            "profile-level-id": "42e01f",
-            "level-asymmetry-allowed": 1
-        },
-        preferredPayloadType: 0
-    }
-  ];
-  
 
-//creating rooms
-const router=await worker.createRouter({mediaCodecs});
 
 //This creates a shared ICE listener.
 //Instead of each transport opening random ports, all clients connect to:9.9.9.9:20000
 
-const webRtcServer = await worker.createWebRtcServer(
-    {
-      listenInfos :
-      [
-        {
-          protocol : 'udp',
-          ip       : '9.9.9.9',
-          //  announcedIp: "YOUR_PUBLIC_IP",,
-          port     : 20000
-        },
-        {
-          protocol : 'tcp',
-          ip       : '9.9.9.9',
-        //  announcedIp: "YOUR_PUBLIC_IP",
-          port     : 20000
-        }
-      ]
-    });
+
+
+
+  
+    
+
+    
