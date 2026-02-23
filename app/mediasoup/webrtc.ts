b8c036926f4161788fde62065b55866ba7e0ed87
Kartik Sharma <poojan4604@gmail.com>
2026-02-23 11:23:04 +0530

    created the producer and also the webrtc server was corrected and some bugs fixed in signaling server

--- app/mediasoup/webrtc.ts
+++ app/mediasoup/webrtc.ts
@@ -4,7 +4,9 @@ import { createWorker } from './worker';
 
 const worker=await createWorker();
 
-
+//A WebRTC server exists within the context of a Worker, meaning that if your app launches N workers it also needs to create N WebRTC servers listening on different ports (to not collide).
+//The WebRTC transport implementation of mediasoup is ICE Lite, meaning that it does not initiate ICE connections but expects ICE Binding Requests from endpoints.
+//WebRtcServerOptions
 export const createWebRTCServer=async()=>{
     const webRtcServer = await worker.createWebRtcServer(
         {
@@ -23,6 +25,9 @@ export const createWebRTCServer=async()=>{
               port     : 20000
             }
           ]
-        });
+        },
+      );
+      console.log("WebRTC Server created:", webRtcServer.id);
+    return webRtcServer;
 }
 
